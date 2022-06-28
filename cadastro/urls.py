2523747ccfe00d33426314c08cb26562effaa698
Victor Santos <victor.vsantos08@gmail.com>
2022-06-28 11:18:06 -0300

    Implementação
    Inclusão de autenticação usando token JWT

--- cadastro/urls.py
+++ cadastro/urls.py
@@ -2,12 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from usuarios.api import viewsets as usuariosviewsets
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 router = routers.DefaultRouter()
 router.register(r'usuarios', usuariosviewsets.UsuariosViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('token/', TokenObtainPairView.as_view()),
+    path('token/refresh/', TokenRefreshView.as_view()),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
 ]
--- usuarios/api/viewsets.py
+++ usuarios/api/viewsets.py
@@ -1,7 +1,9 @@
 from rest_framework import viewsets
 from usuarios.api import serializers
 from usuarios import models
+from rest_framework.permissions import IsAuthenticated
 
 class UsuariosViewSet(viewsets.ModelViewSet):
+  permission_classes = (IsAuthenticated,)
   serializer_class = serializers.UsuariosSerializer
   queryset = models.Usuarios.objects.all()
