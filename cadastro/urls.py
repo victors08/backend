833cd81d37322ee5e3a6fe22df14e96c9e12bbfc
Victor Santos <victor.vsantos08@gmail.com>
2022-07-01 20:17:34 -0300

    Update
    Update nas view do projeto, criação dos métodos GET, POST, PUT, DELETE e dos seus respectivos endpoints.

--- cadastro/urls.py
+++ cadastro/urls.py
@@ -1,16 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from usuarios.api import viewsets as usuariosviewsets
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from usuarios.api.viewsets import UsuariosGet, UsuariosPost, UsuariosDetails
+# from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 router = routers.DefaultRouter()
-router.register(r'usuarios', usuariosviewsets.UsuariosViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('token/', TokenObtainPairView.as_view()),
-    path('token/refresh/', TokenRefreshView.as_view()),
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
+   # path('token/', TokenObtainPairView.as_view()),
+   # path('token/refresh/', TokenRefreshView.as_view()),
+    path('usuarios/', UsuariosGet.as_view()),
+    path('usuarioscreate/', UsuariosPost.as_view()),
+    path('usuarios/<int:id>/', UsuariosDetails.as_view()),
+   # path('api-auth/', include('rest_framework.urls')),
 ]
--- usuarios/api/viewsets.py
+++ usuarios/api/viewsets.py
@@ -1,9 +1,48 @@
-from rest_framework import viewsets
-from usuarios.api import serializers
-from usuarios import models
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import NotFound
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import serializers, status, viewsets
+# from rest_framework.permissions import IsAuthenticated
 
-class UsuariosViewSet(viewsets.ModelViewSet):
-  permission_classes = (IsAuthenticated,)
-  serializer_class = serializers.UsuariosSerializer
-  queryset = models.Usuarios.objects.all()
+from usuarios import models
+from .serializers import UsuariosSerializer
+
+from django.shortcuts import get_object_or_404
+from django.http import JsonResponse, HttpResponse
+import json
+
+
+class UsuariosGet(APIView):
+  def get(self, request):
+    usuarios = models.Usuarios.objects.all()
+    serializer = UsuariosSerializer(usuarios, many = True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+class UsuariosPost(APIView):
+  def post(self, request):
+    serializer = UsuariosSerializer(data=request.data)
+    if serializer.is_valid():
+      serializer.save()
+      return Response(serializer.data, status=status.HTTP_201_CREATED)
+    else:
+      return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class UsuariosDetails(APIView):
+ def get(self, request, id):
+  usuario = get_object_or_404(models.Usuarios.objects.all(), id=id)
+  serializer = UsuariosSerializer(usuario)
+  return Response(serializer.data, status = status.HTTP_200_OK)
+
+def put(self, request, id):
+  usuario = get_object_or_404(models.Usuarios.objects.all(), id=id)
+  serializer = UsuariosSerializer(usuario, data=request.data)
+  if serializer.is_valid():
+    serializer.save()
+    return Response(serializer.data, status=status.HTTP_201_CREATED)
+  else:
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+  
+def delete(self, request, id):
+  usuario = get_object_or_404(models.Usuarios.objects.all(), id=id)
+  usuario.delete()
+  return Response(status=status.HTTP_204_NO_CONTENT)
